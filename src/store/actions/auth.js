fa70e8ed6a808ab56b14ec188a27317f26c63a94
Cathleen Wright <cathleenmwright@gmail.com>
2018-04-19 17:15:17 -0400

    382 - Add JWT to HTTP Headers

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,4 @@
-import {apiCall} from "../../services/api";
+import {apiCall, setTokenHeader} from "../../services/api";
 import {SET_CURRENT_USER} from "../actionTypes";
 import {addError, removeError} from "./errors"
 
@@ -9,9 +9,14 @@ export function setCurrentUser(user) {
   };
 }
 
+export function setAuthorizationToken(token) {
+  setTokenHeader(token);
+}
+
 export function logout() {
   return dispatch => {
     localStorage.clear();
+    setTokenHeader(false);
     dispatch(setCurrentUser({}));
   };
 }
@@ -21,6 +26,7 @@ export function authUser(type, userData) {
     return new Promise((resolve, reject) => {
       return apiCall("post", `/api/auth/${type}`, userData).then(({token, ...user}) => {
         localStorage.setItem("jwtToken", token);
+        setAuthorizationToken(token);
         dispatch(setCurrentUser(user));
         dispatch(removeError());
         resolve();

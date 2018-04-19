380201f0e6c3511ecfb8681730fb56b85ca0eed7
Cathleen Wright <cathleenmwright@gmail.com>
2018-04-19 12:32:38 -0400

    375 - Route exact path typo error, Cleanup

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -9,12 +9,12 @@ const Main = props => {
     <div className="container">
       <Switch>
         <Route exact path="/" render={props => <Homepage {...props}/>}/>
-        <Route exact path="signin" render={props => {
+        <Route exact path="/signin" render={props => {
           return(
             <AuthForm buttonText="Log in" heading="Welcome Back!" {...props} />
           )
         }}/>
-        <Route exact path="signup" render={props => {
+        <Route exact path="/signup" render={props => {
           return(
             <AuthForm buttonText="Sign me up!" heading="Join Warbler Today!" {...props} />
           )

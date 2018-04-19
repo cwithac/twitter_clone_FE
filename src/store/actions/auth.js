dffd4fd813a5f12f679050e6f92a51dfbd5a4675
Cathleen Wright <cathleenmwright@gmail.com>
2018-04-19 15:52:54 -0400

    379 - Handling error messages with redux

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import {apiCall} from "../../services/api";
 import {SET_CURRENT_USER} from "../actionTypes";
+import {addError, removeError} from "./errors"
 
 export function setCurrentUser(user) {
   return {
@@ -14,8 +15,13 @@ export function authUser(type, userData) {
       return apiCall("post", `/api/auth/${type}`, userData).then(({token, ...user}) => {
         localStorage.setItem("jwtToken", token);
         dispatch(setCurrentUser(user));
+        dispatch(removeError());
         resolve();
       })
-    })
-  }
+      .catch(err => {
+        dispatch(addError(err.message));
+        reject();
+      });
+    });
+  };
 }
